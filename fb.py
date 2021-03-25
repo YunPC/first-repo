6837116b82be59a83abe62387fb4f87d689d170a
YunPC <apsn2000@nate.com>
2021-03-25 15:51:13 +0900

    fix: Find Syntax Error, Add print k
    if number is not diveded 3, 5, or 15, print k. In addition, fixed error for

--- fb.py
+++ fb.py
@@ -1,12 +1,14 @@
 def do_fizzbuzz():
 	n = input('please enter the number')
-	for k in n:
+	for k in range(1, n+1):
 		if k % 15 == 0:
 			print('fizzbuzz')
 		elif k % 3 == 0:
 			print('fizz')
 		elif k % 5 == 0:
 			print('buzz')
+		else:
+			print(k)
 
 
 
