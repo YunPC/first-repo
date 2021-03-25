36f8342b90d056f8b95f2359cff19c7e882e3ac9
YunPC <apsn2000@nate.com>
2021-03-25 16:02:10 +0900

    feat: Add do_fizzbuz adversed version
    Using list comprehension, write new fizzbuz function

--- fb.py
+++ fb.py
@@ -11,7 +11,12 @@ def do_fizzbuzz():
 			print(k)
 
 
+def do_fizzbuzz_adversed():
+	n = input('please enter the number : ')
+	answer = ['fizzbuzz' if k % 15 == 0 else 'fizz' if k % 3 == 0 else 'buzz' if k % 5 == 0 else 'buzz' for k in range(1, n+1)]
+	print(answer)
 
 
+do_fizzbuzz_adversed()
 
 do_fizzbuzz()
